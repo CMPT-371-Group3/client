764f4d36569bce73a533f09e34870eb418cf529a
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-19 11:44:52 -0700

    made a slight change

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     //base code..
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Sam says Hello world");
     }
 }
