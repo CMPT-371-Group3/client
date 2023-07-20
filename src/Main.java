3b49f4e947d2b01cee070b36292633c2242d8db1
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-20 00:54:49 -0700

    changes that doesnt matter. delete these anyways

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
-public class Main {
-    //testing
-    //base code..
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("RAWR XD");
-        System.out.println("Sam says Hello world");
+//package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.util.Objects;
+
+public class Main extends Application {
+    public  static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+//        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sceneBuilderFile.fxml")));
+
+//        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        stage.setTitle("Stage demo");
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
