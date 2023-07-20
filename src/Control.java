2e07334e98dcd65b8ece64ca62e129eb9bef0be4
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-20 01:07:53 -0700

    changes that doesnt matter. delete these anyways (#18)

---
+++ src/Control.java
@@ -0,0 +1,17 @@
+import javafx.event.ActionEvent;
+
+public class Control {
+
+        public void UP(ActionEvent e){
+                System.out.println("UP");
+        }
+        public void D(ActionEvent e){
+                System.out.println("Down");
+        }
+        public void R(ActionEvent e){
+                System.out.println("right");
+        }
+        public void Left(ActionEvent e){
+                System.out.println("Left");
+        }
+}
