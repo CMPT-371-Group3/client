8950fd5b117e77a990777ab4e67a689112677b87
shawnh29 <81540655+shawnh29@users.noreply.github.com>
2023-07-21 20:47:08 -0700

    added skeleton game board, end screen (#20)
    * added skeleton game board, end screen
    
    * deleted unneeded files

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,11 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Scenes/Main.fxml"));
 
         stage.setTitle("Deny and Conquer: Main Menu");
         stage.setScene(new Scene((root)));
-        Image icon = new Image("garfield_deny.jpg");
+        Image icon = new Image("Scenes/garfield_deny.jpg");
         stage.getIcons().add(icon);
         stage.show();
     }
--- src/SceneController.java
+++ src/SceneController.java
@@ -16,22 +16,42 @@ public class SceneController {
     private Parent root;
 
     public void switchToMainPage(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        root = FXMLLoader.load(getClass().getResource("Scenes/Main.fxml"));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        Image icon = new Image("garfield_deny.jpg");
+        Image icon = new Image("Scenes/garfield_deny.jpg");
         stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToHostWaiting(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Host_Waiting_Screen.fxml"));
+        root = FXMLLoader.load(getClass().getResource("Scenes/Host_Waiting_Screen.fxml"));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        Image icon = new Image("garfield_deny.jpg");
+        Image icon = new Image("Scenes/garfield_deny.jpg");
         stage.getIcons().add(icon);
         stage.setTitle("Deny and Conquer: Waiting Room");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToEndPage(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scenes/End_Screen.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        Image icon = new Image("Scenes/garfield_deny.jpg");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToGameBoard(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scenes/Game_Board.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        Image icon = new Image("Scenes/garfield_deny.jpg");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
