e7f14d3a66ce344c244fcb19b3f1aa8e62344ff8
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-24 01:53:15 -0700

    made the first 4 grids be able to draw. need to do the rest

--- src/SceneController.java
+++ src/SceneController.java
@@ -4,8 +4,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+//import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.paint.*;
 
 import java.io.IOException;
 
@@ -14,6 +19,23 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+//    private Canvas canvas;
+
+    //adding a listener to be able to draw on the canvas
+    //called automatically by the fxml loader
+//    public void initialize() {
+//        GraphicsContext g = canvas.getGraphicsContext2D();
+//        canvas.setHeight(20);
+
+//        canvas.setOnMouseDragged(e -> {
+//            double size = 5.0;
+//            double x = e.getX() - size / 2;
+//            double y = e.getY() - size / 2;
+//
+//            g.setFill(Color.RED);
+//            g.fillRect(x, y, size, size);
+//        });
+//    }
 
     public void switchToMainPage(ActionEvent e) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scenes/Main.fxml"));
@@ -55,4 +77,5 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+
 }
