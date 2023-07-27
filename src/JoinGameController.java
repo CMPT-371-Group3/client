2b47dd18625d3e6d7f3a9d7b064af8ce66bbcb06
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-27 12:58:43 -0700

    26 work on host game room scene (#29)
    * made the screen and did initial setup of the page.
    Also made waiting screen display the real ip address
    
    * made the screen and did initial setup of the page.
    Also made waiting screen display the real ip address
    
    * tried to start connection, but didnt do it
    
    * Fixed painting bug: didnt detect 50% correctly
    
    * Fixed painting bug: didnt detect 50% correctly

---
+++ src/JoinGameController.java
@@ -0,0 +1,63 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+//import java.awt.*;
+//join game
+import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class JoinGameController {
+
+    @FXML
+    private TextField inputText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMainPage(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scenes/Main.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        Image icon = new Image("Scenes/garfield_deny.jpg");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+//    public void joinGameBTN(ActionEvent e)
+
+    public void switchToHostWaiting(ActionEvent e) throws IOException {
+
+        //HAVE TO DO MORE WORK HERE TO CHECK - IF CAN FIND THE CONNECTION
+//        String ipAddress = inputText.getText();
+//
+//        makeConnection(ipAddress);
+
+
+        root = FXMLLoader.load(getClass().getResource("Scenes/Host_Waiting_Screen.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        Image icon = new Image("Scenes/garfield_deny.jpg");
+        stage.getIcons().add(icon);
+        stage.setTitle("Deny and Conquer: Waiting Room");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void makeConnection(String ipAddress){
+        try {
+            Socket MySocket = new Socket(ipAddress, 7070);
+//            OutputStream os
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
