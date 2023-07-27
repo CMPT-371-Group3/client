4fde5ebd249e42c47b3bd8360f063e32d7aac49c
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-07-26 21:52:14 -0700

    tried to start connection, but didnt do it

--- src/JoinGameController.java
+++ src/JoinGameController.java
@@ -7,11 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class JoinGameController {
 
     @FXML
+    private TextField inputText;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -24,5 +28,34 @@ public class JoinGameController {
         stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
+    }
+
+//    public void joinGameBTN(ActionEvent e)
+
+    public void switchToHostWaiting(ActionEvent e) throws IOException {
+
+        //HAVE TO DO MORE WORK HERE TO CHECK - IF CAN FIND THE CONNECTION
+        String ipAddress = inputText.getText();
+
+        makeConnection(ipAddress);
+
+
+        root = FXMLLoader.load(getClass().getResource("Scenes/Host_Waiting_Screen.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        Image icon = new Image("Scenes/garfield_deny.jpg");
+        stage.getIcons().add(icon);
+        stage.setTitle("Deny and Conquer: Waiting Room");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void makeConnection(String ipAddress){
+        try {
+            Socket MySocket = new Socket(ipAddress, 7070);
+//            OutputStream os
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
