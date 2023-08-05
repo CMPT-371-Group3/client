5a237a459386feb71da701f6e82ed93f6709c975
shawnh29 <81540655+shawnh29@users.noreply.github.com>
2023-08-04 17:56:50 -0700

    assigning colors, using input to open sockets, misc. (#41)

---
+++ src/MainScreenController.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainScreenController {
+
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToJoinGame(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scenes/JoinGame.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Join Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/SceneController.java
+++ src/SceneController.java
@@ -4,14 +4,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.*;
-import javafx.scene.canvas.*;
-//import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.scene.paint.*;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
@@ -29,9 +24,10 @@ public class SceneController {
     private Button readyBtn;
 
     public void initialize() {
+        System.out.println("Calling initialize..");
         Client.getInstance().threadedListening();
 
-        if(ipLabel != null){ //code for the waiting screen and not the main screen
+        if (ipLabel != null) { //code for the waiting screen and not the main screen
             try {
                 String ipAddress = Inet4Address.getLocalHost().getHostAddress();
                 ipLabel.setText("Your Host IP Address: " + ipAddress);
@@ -52,25 +48,6 @@ public class SceneController {
         stage.show();
     }
 
-    public void switchToHostWaiting(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scenes/Host_Waiting_Screen.fxml"));
-        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        Image icon = new Image("Scenes/garfield_deny.jpg");
-        stage.getIcons().add(icon);
-        stage.setTitle("Deny and Conquer: Waiting Room");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToJoinGame(ActionEvent e) throws  IOException {
-        root = FXMLLoader.load(getClass().getResource("Scenes/JoinGame.fxml"));
-        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setTitle("Join Game");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public void switchToEndPage(ActionEvent e) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scenes/End_Screen.fxml"));
@@ -85,6 +62,7 @@ public class SceneController {
     public void readyButtonPressed(ActionEvent e) throws IOException {
         readyBtn.setDisable(true);
         readyBtn.setText("Waiting");
+        Client.getInstance().sendMessage("READY");
     }
 
     public void switchToGameBoard(ActionEvent e) throws IOException {
@@ -97,5 +75,24 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchToJoinGame(ActionEvent e) throws  IOException {
+        root = FXMLLoader.load(getClass().getResource("Scenes/JoinGame.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Join Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+    //    public void switchToHostWaiting(ActionEvent e) throws IOException {
+//        root = FXMLLoader.load(getClass().getResource("Scenes/Host_Waiting_Screen.fxml"));
+//        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        Image icon = new Image("Scenes/garfield_deny.jpg");
+//        stage.getIcons().add(icon);
+//        stage.setTitle("Deny and Conquer: Waiting Room");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
 }
